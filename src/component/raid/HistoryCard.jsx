aa6f6ebac99f674cb0b0b2022cbdd378b774c4ea
hyejin <monlang@naver.com>
2024-08-23 16:38:22 +0900

    [Design] 레이드 참여내역 css 수정

--- src/component/raid/HistoryCard.jsx
+++ src/component/raid/HistoryCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import 'asset/css/RaidHistory.css'
 
-function HistoryCard({ image, merchantName, contribution, isSuccess, startTime, endTime, isRewarded }) {
-    
+function HistoryCard({ image, merchantName, contribution, percentage, isSuccess, startTime, endTime, isRewarded }) {
+    const today =new Date();
+
     function formatTimestamp(timestamp) {
         const date = new Date(timestamp);
     
@@ -13,33 +14,37 @@ function HistoryCard({ image, merchantName, contribution, isSuccess, startTime, 
     
         return `${month}/${day} ${hours}:${minutes}`;
     }
-    
     function formatNumber(number) {
         return number.toLocaleString("ko-KR");
     }
+
+    const raidStatus = 
+    isSuccess === 1 
+        ? "성공" : new Date(endTime) > today
+            ? "진행중": "실패";
     
     return (
-        <div className="card">
-            <img src={image} alt={merchantName} className="card-image" />
-            <div className="card-content">
-                <div className="card-header">
+        <div className="raid-card">
+            <div className="raid-card-content">
+                <div className="raid-card-header">
+                    <img src={image} alt={merchantName} className="raid-card-image" />
                     <h3>{merchantName}</h3>
-                    <button onClick={(e) => console.log("click!")} className={`card-button ${isRewarded===0 ? 'fail' : isRewarded===1?'':'received'}`} disabled={isRewarded===1?'':'disabled'}>
+                    <button onClick={(e) => console.log("click!")} className={`raid-card-button ${isRewarded===0 ? 'fail' : isRewarded===1?'':'received'}`} disabled={isRewarded===1?'':'disabled'}>
                         {isRewarded===0 ? '대상 아님' : isRewarded===1?"보상 받기":"수령 완료"}
                     </button>
                 </div>
-                <div className="card-info">
+                <div className="raid-card-info">
                     <div>
                         <p>기여도</p>
-                        <p className="amount">{formatNumber(contribution)}</p>
+                        <p className="raid-info-detail">{formatNumber(contribution)}({percentage}%)</p>
                     </div>
                     <div>
                         <p>결과</p>
-                        <p>{isSuccess===1?"실패":"성공"}</p>
+                        <p className="raid-info-detail">{raidStatus}</p>
                     </div>
                     <div>
                         <p>기간</p>
-                        <p>{formatTimestamp(startTime)} ~ {formatTimestamp(endTime)}</p>
+                        <p className="raid-info-detail">{formatTimestamp(startTime)} <br/>{formatTimestamp(endTime)}</p>
                     </div>
                 </div>
             </div>
